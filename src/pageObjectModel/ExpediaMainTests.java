4e23f83791380118e2d883b9ecdec1e16de1fb16
rjtbansal <rjtbansal@yahoo.com>
2019-01-27 15:31:43 -0800

    example for page object model

---
+++ src/pageObjectModel/ExpediaMainTests.java
@@ -0,0 +1,59 @@
+package pageObjectModel;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pageObjectModel.pageclasses.FlightsTab;
+
+//expedia website main tests go here
+public class ExpediaMainTests {
+
+	private WebDriver driver;
+	private String base_url;
+	
+	@Before
+	public void setUp() throws Exception {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\rjtba\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		
+		base_url = "https://www.expedia.com";
+		
+		
+		driver.get(base_url);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+//		//close banner
+//		WebElement remove_banner_button = FlightsTab.removeTopBannerButton(driver);
+//		if(remove_banner_button.isDisplayed()) {
+//			remove_banner_button.click();
+//		}
+		
+		
+	}
+	
+	@Test
+	public void testFlightSearch() throws Exception {
+		FlightsTab.clickFlightsTab(driver);
+		FlightsTab.clickRoundTripTab(driver);
+		FlightsTab.inputFlyingFromField(driver, "New York");
+		FlightsTab.inputFlyingToField(driver, "Toronto");
+		FlightsTab.inputDepartingDateField(driver, "28/01/2019");
+		FlightsTab.returningDateField(driver).clear();
+		//FlightsTab.inputReturningDateField(driver, "11/01/2019");
+		FlightsTab.returningDateField(driver).sendKeys("31/01/2019");
+		FlightsTab.clickSearchButton(driver);
+		Thread.sleep(2000);
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		Thread.sleep(2000);
+		driver.quit();
+	}
+}
---
+++ src/pageObjectModel/pageclasses/FlightsTab.java
@@ -0,0 +1,113 @@
+package pageObjectModel.pageclasses;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+//locators and methods for flights tab on expedia website
+public class FlightsTab {
+
+	public static WebElement element = null;
+	
+	public static WebElement removeTopBannerButton(WebDriver driver) {
+		return driver.findElement(By.id("join-rewards-close-btn"));
+	}
+	
+	//flights tab locator
+	public static WebElement flightsTab(WebDriver driver) {
+		
+		element = driver.findElement(By.id("tab-flight-tab-hp"));
+		return element;
+	}
+	
+	public static void clickFlightsTab(WebDriver driver) {
+		element = flightsTab(driver);
+		element.click();
+	}
+	
+	//round-trip sub tab
+	public static WebElement roundTripTab(WebDriver driver) {
+		element = driver.findElement(By.id("flight-type-roundtrip-label-hp-flight"));
+		return element;
+	}
+	
+	public static void clickRoundTripTab(WebDriver driver) {
+		element = roundTripTab(driver);
+		element.click();
+	}
+	
+	//flying from field
+	public static WebElement flyingFromField(WebDriver driver) {
+		return driver.findElement(By.id("flight-origin-hp-flight"));
+	}
+	
+	public static void inputFlyingFromField(WebDriver driver, String flying_from_airport) throws InterruptedException {
+		element = flyingFromField(driver);
+		element.sendKeys(flying_from_airport);
+		WebElement dropdown_airport_list_menu = driver.findElement(By.id("typeaheadDataPlain"));
+		List<WebElement> airport_list = dropdown_airport_list_menu.findElements(By.tagName("a"));
+		for(WebElement airport : airport_list) {
+			if(airport.getAttribute("data-value").contains("JFK-John F. Kennedy")) {
+				airport.click();
+				break;
+			}
+		}
+		Thread.sleep(3000);
+	}
+	
+	//flying to field
+	public static WebElement flyingToField(WebDriver driver) {
+		return driver.findElement(By.id("flight-destination-hp-flight"));
+	}
+	
+	public static void inputFlyingToField(WebDriver driver, String flying_to_airport) throws InterruptedException {
+		element = flyingToField(driver);
+		element.sendKeys(flying_to_airport);
+		WebElement dropdown_airport_list_menu = driver.findElement(By.id("typeaheadDataPlain"));
+		List<WebElement> airport_list = dropdown_airport_list_menu.findElements(By.tagName("a"));
+		for(WebElement airport : airport_list) {
+			if(airport.getAttribute("data-value").contains("Toronto (and vicinity), Ontario")) {
+				airport.click();
+				break;
+			}
+		}
+		Thread.sleep(3000);
+	}
+	
+	//departing field
+	public static WebElement departingDateField(WebDriver driver) {
+		return driver.findElement(By.id("flight-departing-hp-flight"));
+	}
+	
+	public static void inputDepartingDateField(WebDriver driver, String departing_date) {
+		element = departingDateField(driver);
+		element.sendKeys(departing_date);
+	}
+	
+	//returning field
+	public static WebElement returningDateField(WebDriver driver) {
+		return driver.findElement(By.id("flight-returning-hp-flight"));
+	}
+	
+	public static void inputReturningDateField(WebDriver driver, String return_date) {
+		element = returningDateField(driver);
+		element.sendKeys(return_date);
+	}
+	
+	//search button
+	public static WebElement searchButton(WebDriver driver) {
+		return driver.findElement(By.xpath("//form[@id='gcw-flights-form-hp-flight']//button[@type='submit']//span"));
+	}
+	
+	public static void clickSearchButton(WebDriver driver) {
+		element = searchButton(driver);
+		element.click();
+	}
+	
+	
+	
+	
+	
+}
