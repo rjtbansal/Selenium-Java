e9837726074517e7f9f01e1166507374f18b431f
rjtbansal <rjtbansal@yahoo.com>
2019-02-10 13:38:11 -0800

    testng asserts: added useful comments

--- src/testNgClasses/TestNg_Asserts.java
+++ src/testNgClasses/TestNg_Asserts.java
@@ -7,20 +7,32 @@ import testNgClassesToTest.MathTestClass;
 
 public class TestNg_Asserts {
 	//Test annotation means below method is a test case
+	
+	/*@Test cases run in an alphabetical order and not the order in which they are coded
+	 * 
+	 * Here execution order would be : testArrays -> testStrings -> testSum
+	 * 
+	 * */
   @Test
   public void testSum() {
 	  
-	  System.out.println("Running testSum test case");
+	  System.out.println("\n Running testSum test case");
 	  MathTestClass mathObj = new MathTestClass();
 	  int sum = mathObj.addNumbers(12, 1);
 	  //Assert.assertEquals(sum, 13);
 	  //assert conditions specifies if result of actual matches with expected and based on condition test passes/fails
-	  Assert.assertNotEquals(sum, 13);
+	  Assert.assertNotEquals(sum, 10);
+	  
+	  /*below statement wont execute if assertion above fails
+	   * This is typically Hard Assertion by default. In hard assertion if an assertion fails all code below that assertion for that test method wont execute at all
+	   * */
+	 
+	  System.out.println("\ntestSum method after assertion");
   }
   
   @Test
   public void testStrings() {
-	  System.out.println("Running testStrings test case");
+	  System.out.println("\n Running testStrings test case");
 	  MathTestClass mathObj = new MathTestClass();
 	  String added_strings = mathObj.addStrings("hello", "world");
 	  Assert.assertEquals(added_strings, "hello world");
@@ -28,7 +40,7 @@ public class TestNg_Asserts {
   
   @Test
   public void testArrays() {
-	  System.out.println("Running testArrays method");
+	  System.out.println("\n Running testArrays method");
 	  int[] expected_array = {2,2,1};
 	  MathTestClass mathObj = new MathTestClass();
 	  int[] actual_array=mathObj.getArray();
