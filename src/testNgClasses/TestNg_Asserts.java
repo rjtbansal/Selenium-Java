7400635e3f09aabdd84c5ae92a07d6557bd24f3a
rjtbansal <rjtbansal@yahoo.com>
2019-02-10 14:34:01 -0800

    added soft asserts method example

--- src/testNgClasses/TestNg_Asserts.java
+++ src/testNgClasses/TestNg_Asserts.java
@@ -2,6 +2,7 @@ package testNgClasses;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import testNgClassesToTest.MathTestClass;
 
@@ -21,13 +22,31 @@ public class TestNg_Asserts {
 	  int sum = mathObj.addNumbers(12, 1);
 	  //Assert.assertEquals(sum, 13);
 	  //assert conditions specifies if result of actual matches with expected and based on condition test passes/fails
-	  Assert.assertNotEquals(sum, 10);
+	  Assert.assertNotEquals(sum, 13);
 	  
-	  /*below statement wont execute if assertion above fails
+	  /*below statement wont execute if assertion above fails and it will throw an exception.
 	   * This is typically Hard Assertion by default. In hard assertion if an assertion fails all code below that assertion for that test method wont execute at all
 	   * */
 	 
 	  System.out.println("\ntestSum method after assertion");
+  }
+  
+  @Test
+  public void testSumSoftAssert() {
+	 /*
+	  * SOft Asserts: In this method we are using Soft Assert. With SOft Assert even when an assert fails, the following code continues executing. If we really want to throw an exception and stop the further execution of the testcase,
+	  * then at the end of the method use assertAll
+	  */
+	  System.out.println("\n Running testSumSoftAssert test case");
+	  MathTestClass mathObj = new MathTestClass();
+	  int sum = mathObj.addNumbers(12, 1);
+	  
+	  SoftAssert sa = new SoftAssert(); //to use soft assert explicitly instantiate its object
+	  sa.assertEquals(sum, 10); //use soft assert object to access its methods
+	  System.out.println("----After first assertEqual----");
+	  sa.assertNotEquals(sum, 13);
+	  System.out.println("----After second assertEqual----");
+	  //sa.assertAll();  //if we want to throw exception on failed assert and dont want the code to continue executing, use assertAll at the end of the test case
   }
   
   @Test
