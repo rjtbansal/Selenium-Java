492504180d5072b63644301e815e9b02ad65f18e
rjtbansal <rjtbansal@yahoo.com>
2019-02-10 22:30:20 -0800

    using xml file to run testsuites

---
+++ src/testNgClasses/TestNg_TestClass1.java
@@ -0,0 +1,40 @@
+package testNgClasses;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class TestNg_TestClass1 {
+	@BeforeMethod
+	  public void beforeMethod() {
+		  System.out.println("TestNg_TestClass1 ----> Runs before every method");
+	  }
+
+	  @AfterMethod
+	  public void afterMethod() {
+		  System.out.println("TestNg_TestClass1 ----> Runs after every method");
+	  }
+
+	  @BeforeClass
+	  public void beforeClass() {
+		  System.out.println("TestNg_TestClass1 ----> Runs once before class");
+	  }
+
+	  @AfterClass
+	  public void afterClass() {
+		  System.out.println("TestNg_TestClass1 ----> Runs once after class");
+	  }
+
+	  @Test
+	  public void testMethod1() {
+	    System.out.println("TestNg_TestClass1 ----> Running testMethod1");
+	  }
+	  
+	  @Test
+	  public void testMethod2() {
+	    System.out.println("TestNg_TestClass1 ----> Running testMethod2");
+	  }
+
+}
