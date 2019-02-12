c317bc4639a55d8f6b296b4cd7250d03e6e7b426
rjtbansal <rjtbansal@yahoo.com>
2019-02-11 23:30:24 -0800

    test groups example

---
+++ src/testNgClasses/TestNgGrouping.java
@@ -0,0 +1,41 @@
+package testNgClasses;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class TestNgGrouping {
+ 
+  @BeforeClass
+  public void beforeClass() {
+	  System.out.println("Before class method");
+  }
+  
+  //groups specified in test argument are similar to tags in robot framework. We can group tests to run specific tests instead of running all the tests in a testsuite
+  //we need to specify which groups to run in an xml file : here its testng-groups.xml
+  @Test(groups= {"cars","suv"})
+  public void testHondaCRV() {
+	  System.out.println("Running test -> Honda CRV");
+  }
+  
+  @Test(groups= {"cars","sedan"})
+  public void testAudiA6() {
+		System.out.println("Running Test - Audi A6 ");
+	}
+  
+  @Test(groups= {"bikes"})
+  public void testNinja() {
+		System.out.println("Running Test - Kawasaki Ninja");
+	}
+  
+  @Test(groups= {"bikes"})
+  public void testHondaCBR() {
+		System.out.println("Running Test - Honda CBR");
+	}
+
+  @AfterClass
+  public void afterClass() {
+	  System.out.println("After class method");
+  }
+
+}
