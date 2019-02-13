dacaf41f24700bd769d0581f949a1fd4a88beafb
rjtbansal <rjtbansal@yahoo.com>
2019-02-12 22:51:27 -0800

    enable timeout example

---
+++ src/testNgClasses/TestNg_EnableTimeout.java
@@ -0,0 +1,30 @@
+package testNgClasses;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestNg_EnableTimeout {
+	@BeforeClass
+	public void setUp() {
+		System.out.println("before class");
+	}
+	
+	@AfterClass
+	public void cleanUp() {
+		System.out.println("after class");
+	}
+	
+	//enabled false will disable the below test method to run. useful when debugging if we want to disable it
+	@Test(enabled=false)
+	public void testMethod1() {
+		System.out.println("testMethod1");
+	}
+	
+	//timeout means the time this test should take to execute. if timeout exceeds exception is thrown
+	@Test(timeOut=300)
+	public void testMethod2() throws InterruptedException {
+		System.out.println("testMethod2");
+		Thread.sleep(200);
+	}
+}
